ddfcf09c7c7f816d5d8e97a067fec1c4ad9cbfa8
Vasily Vasilyev <vasar007@yandex.ru>
2020-06-23 13:06:05 +0300

    Add no op non-final stateful task to V1 and V2 of StateMachine

---
+++ Items/Libraries/Items.StateMachine/V1/States/NoOpNonFinalStatefulTask.cs
@@ -0,0 +1,22 @@
+﻿namespace Items.StateMachine.V1.States
+{
+    public sealed class NoOpNonFinalStatefulTask<TState> : NonFinalStatefulTaskBase<TState>
+        where TState : class
+    {
+        private readonly IStatefulTask<TState> _stateToMove;
+
+        public NoOpNonFinalStatefulTask(IStatefulTask<TState> stateIdToMove)
+        {
+            _stateToMove = stateIdToMove;
+        }
+
+        #region NonFinalStatefulTaskBase<TState> Overridden Methods
+
+        protected override IStatefulTask<TState> DoActionInternal(TState state)
+        {
+            return _stateToMove;
+        }
+
+        #endregion
+    }
+}
---
+++ Items/Libraries/Items.StateMachine/V2/States/NoOpNonFinalStatefulTask.cs
@@ -0,0 +1,33 @@
+﻿namespace Items.StateMachine.V2.States
+{
+    public sealed class NoOpNonFinalStatefulTask<TState, TStateId>
+        : NonFinalStatefulTaskBase<TState, TStateId>
+        where TState : class
+    {
+        private readonly TStateId _stateIdToMove;
+
+        public NoOpNonFinalStatefulTask(TStateId stateIdToMove)
+        {
+            _stateIdToMove = stateIdToMove;
+        }
+
+        #region NonFinalStatefulTaskBase<TState, TStateId> Overridden Methods
+
+        protected override TStateId DoActionInternal(TState state)
+        {
+            return _stateIdToMove;
+        }
+
+        #endregion
+    }
+
+    public static class NoOpNonFinalStatefulTask<TState>
+        where TState : class
+    {
+        public static NoOpNonFinalStatefulTask<TState, TStateId> Create<TStateId>(
+            TStateId stateIdToMove)
+        {
+            return new NoOpNonFinalStatefulTask<TState, TStateId>(stateIdToMove);
+        }
+    }
+}
