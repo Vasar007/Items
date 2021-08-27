f1e6caa0286b0eac1d9336813c55972d50f44da3
Vasily Vasilyev <vasily.vasilyev@veeam.com>
2021-08-28 00:46:04 +0200

    Features: use macros in path and add PerformStraightforward method

--- Items/Libraries/Items.StateMachine/V4/StateMachineHelper.cs
+++ Items/Libraries/Items.StateMachine/V4/StateMachineHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Items.Common.Logging;
 using Items.StateMachine.V4.Tasks;
+using Items.StateMachine.V4.Tasks.Straightforward;
 
 namespace Items.StateMachine.V4
 {
@@ -35,6 +36,40 @@ namespace Items.StateMachine.V4
                 // Perform the final task.
                 Logger.Debug($"Executing task: {currentTask}.");
                 currentTask.DoAction(context);
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex, "Exception occurred during state machine performing.");
+            }
+
+            Logger.Debug($"Final state: {context}");
+            return context;
+        }
+
+        public static TContext PerformStraightforward<TContext>(
+            TContext context,
+            IReadOnlyList<IStraightforwardStatefulTask<TContext>> tasks)
+        {
+            // We can log type names of state and tasks but it will be helpful for debugging.
+            Logger.Debug($"Initial state: {context}");
+
+            try
+            {
+                Logger.Debug("Starting performing.");
+
+                foreach (IStraightforwardStatefulTask<TContext> task in tasks)
+                {
+                    Logger.Debug($"Executing task: {task}.");
+
+                    _ = task.DoAction(context);
+                    Logger.Debug($"Current state: {context}.");
+
+                    // On final task break execution.
+                    if (task.IsFinal)
+                    {
+                        break;
+                    }
+                }
             }
             catch (Exception ex)
             {
